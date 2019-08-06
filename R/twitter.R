0ac8f89c2c1b0ecdd7115a54c61d4d8c77dbe714
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-08-06 15:33:43 -0300

    refatora funcao para baixar por apelidos

--- R/twitter.R
+++ R/twitter.R
@@ -1,22 +1,27 @@
 library(rtweet)
 library(lubridate)
 library(tidyverse)
+library(tibble)
 
-get_tweets_pls <- function(words_df) {
-  tweets <- purrr::map_df(words_df$termos, ~ .get_tweets(.x))
-  
+get_tweets_pls_apelido <- function(words_df) {
+  tweets <- purrr::map_df(words_df$apelido, ~ .get_tweets(.x))
 }
 
 .get_tweets <- function(word) {
   print(paste0("Baixando tweets com termo: '", word, "'..."))
   tweets <- search_tweets(word, n = 18000, include_rts = FALSE)
-  tweets <- tweets %>% 
-    mutate(termo = word) %>% 
-    mutate(week = epiweek(created_at))
-  pressao <- tweets %>%
-    group_by(termo, week) %>%
-    summarise(tweets = n(),
-              retweets = sum(retweet_count),
-              favs = sum(favorite_count))
+  pressao <- as_tibble()
+  if (nrow(tweets) != 0) {
+    tweets <- tweets %>% 
+      mutate(termo = word) %>% 
+      mutate(week = epiweek(created_at))
+    pressao <- tweets %>%
+      group_by(termo, week) %>%
+      summarise(tweets = n(),
+                retweets = sum(retweet_count),
+                favs = sum(favorite_count))  
+  } else {
+    warning(paste0("Nao possui tweets relacionados ao termo: '", word, "'."))
+  }
+  return(pressao)
 }
-
